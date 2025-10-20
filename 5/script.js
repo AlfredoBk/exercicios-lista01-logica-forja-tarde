572042ef4bbf89872c8a9af1c9c76715f7f7130d
AlfredoBk <garciabaca0711@gmail.com>
2025-10-20 16:27:13 -0300

    Add files via upload

---
+++ 5/script.js
@@ -0,0 +1,38 @@
+// console.log(fruta)
+
+var fruta = "morango"
+
+console.log(fruta)
+
+//console.log(variavel)
+
+const nome = "alfredo"
+console.log(nome)
+
+//lupin,
+
+let lala
+lala = "cadeira"
+console.log(lala)
+
+//lolo
+
+  var numero = 2
+  numero = "Magarita"
+  console.log(numero)
+  
+  //peperoni
+
+  const frase = "it´s  time to rock"
+  console.log(frase)
+  
+//5263.2
+
+let num = "2"
+console.log(num, typeof num)
+
+//5263.ç
+
+let nu = "2"
+num = 2
+console.log(num, typeof num)
